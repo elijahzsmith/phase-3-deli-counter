537f0a7ed54fdd1e335660b071f03f41cfa66ae8
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-25 12:51:40 -0500

    commit tree a548be5d0b6c9e3f04885d49a5516dae057c4fb4

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+katz_deli = []
+
+def line (katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        the_line = "The line is currently:"
+        katz_deli.map.with_index(1) do |name, index|
+            the_line << " #{index}. #{name}" 
+        end
+        puts the_line
+    end     
+end
+
+
+def take_a_number (katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
